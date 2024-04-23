831f0ca85d3695db0cd3a2f994c3901530277418
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-04-23 10:59:11 +0200

    register view

--- videoflix_backend/urls.py
+++ videoflix_backend/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from videoflix.views import LoginView
+from videoflix.views import LoginView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
 ]
