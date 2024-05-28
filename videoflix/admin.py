2be21c60413b2e0708c5f0e2ca53c58b0cc464e0
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-05-28 10:33:26 +0200

    import export false

--- videoflix/admin.py
+++ videoflix/admin.py
@@ -5,12 +5,13 @@ from import_export.admin import ImportExportModelAdmin
 from videoflix.models import Video
 
 # Register your models here.
+admin.site.register(Video)
 
-class VideoResource(resources.ModelResource):
-    class Meta:
-        model = Video
+# class VideoResource(resources.ModelResource):
+#     class Meta:
+#         model = Video
 
-class VideoAdmin(ImportExportModelAdmin):
-    pass
+# class VideoAdmin(ImportExportModelAdmin):
+#     pass
 
-admin.site.register(Video, VideoAdmin)
+# admin.site.register(Video, VideoAdmin)
--- videoflix_backend/urls.py
+++ videoflix_backend/urls.py
@@ -23,8 +23,8 @@ from videoflix.views import ForgottenPasswordView, LoginView, RegisterView, Acti
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('videoflix_backend/', include('videoflix_backend.urls')),
-    path('admin/', admin.site.urls, name='admin'),
+    # path('videoflix_backend/', include('videoflix_backend.urls')),
+    path('admin/', admin.site.urls),
     path('', LoginView.as_view()),
     path('register/', RegisterView.as_view()),
     path('account_activation/', ActivateNewAccountView.as_view()),
