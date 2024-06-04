8d5cfbe1e719b29fbca2efb6ada2bd5f343a9313
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-06-04 12:19:41 +0200

    shell true

--- videoflix/tasks.py
+++ videoflix/tasks.py
@@ -6,7 +6,7 @@ def convert_1080p(source):
     converted_file_name = source_without_ending + '_1080p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, capture_output=True)
 
 
 # converts uploaded video to 720p
@@ -15,7 +15,7 @@ def convert_720p(source):
     converted_file_name = source_without_ending + '_720p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, capture_output=True)
 
 
 # converts uploaded video to 480p
@@ -24,4 +24,4 @@ def convert_480p(source):
     converted_file_name = source_without_ending + '_480p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, capture_output=True)
