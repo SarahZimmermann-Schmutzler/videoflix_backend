ecf707c0bcb51d3cf081e52ea544674669807942
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-06-04 14:18:36 +0200

    sudo ffmpeg

--- videoflix/tasks.py
+++ videoflix/tasks.py
@@ -4,7 +4,8 @@ import subprocess
 def convert_1080p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_1080p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    # cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'sudo ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
     subprocess.run(cmd, shell=True, capture_output=True)
 
@@ -13,7 +14,8 @@ def convert_1080p(source):
 def convert_720p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_720p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    # cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'sudo ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
     subprocess.run(cmd, shell=True, capture_output=True)
 
@@ -22,6 +24,7 @@ def convert_720p(source):
 def convert_480p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_480p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    # cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'sudo ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
     subprocess.run(cmd, shell=True, capture_output=True)
