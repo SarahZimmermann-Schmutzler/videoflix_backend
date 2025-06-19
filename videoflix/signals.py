f4416e8bf4a11cac873e1fdfc1a9b3c40dd933bc
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2025-06-19 10:52:21 +0200

    signals smtlib

--- videoflix/signals.py
+++ videoflix/signals.py
@@ -79,8 +79,10 @@ def activate_account(sender, instance, created, **kwargs):
         em['To']=email_receiver
         em['Subject']=subject
         em.set_content(body)
+        
         context=ssl.create_default_context()
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.starttls(context=context)
             smtp.login(email_sender, email_password)
             smtp.sendmail(email_sender, email_receiver, em.as_string())
