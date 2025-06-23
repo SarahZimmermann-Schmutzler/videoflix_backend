ae88aedf2edcc9758db042503e108316207a0c39
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2025-06-23 11:19:34 +0200

    signals.py no sudo

--- videoflix/tasks.py
+++ videoflix/tasks.py
@@ -7,9 +7,12 @@ def convert_1080p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_1080p.mp4'
     # cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
-    cmd = 'sudo ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, shell=True, capture_output=True)
+    result = subprocess.run(cmd, shell=True, capture_output=True)
+    
+    if result.returncode != 0:
+        print("Fehler bei FFmpeg_1080p:", result.stderr)
 
 
 def convert_720p(source):
@@ -19,9 +22,12 @@ def convert_720p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_720p.mp4'
     # cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
-    cmd = 'sudo ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, shell=True, capture_output=True)
+    result = subprocess.run(cmd, shell=True, capture_output=True)
+    
+    if result.returncode != 0:
+        print("Fehler bei FFmpeg_720p:", result.stderr)
 
 
 def convert_480p(source):
@@ -31,6 +37,9 @@ def convert_480p(source):
     source_without_ending = source.replace('.mp4', '')
     converted_file_name = source_without_ending + '_480p.mp4'
     # cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
-    cmd = 'sudo ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
+    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, converted_file_name)
     print(source_without_ending,converted_file_name,cmd)
-    subprocess.run(cmd, shell=True, capture_output=True)
+    result = subprocess.run(cmd, shell=True, capture_output=True)
+
+    if result.returncode != 0:
+        print("Fehler bei FFmpeg_480p:", result.stderr)
